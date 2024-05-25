bab1235d82056b116b45d83559a2d0b1e1ac51ae
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2024-05-25 18:42:13 -0300

    Update Loja.java

--- engenhariadesoftware/loja/loja/src/main/java/com/example/Loja.java
+++ engenhariadesoftware/loja/loja/src/main/java/com/example/Loja.java
@@ -1,46 +1,46 @@
 package com.example;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedList; import java.util.List;
 
 public class Loja {
-        public List<Celular> celulares = new LinkedList<
-        public List<Tablet> tablets = new LinkedList<
 
-    
-         public void cadastrarCelular(String modeloCelular, String marcaCelular, int qntdCelular){
-         Celular celular = new Celular(modeloCelular, marcaCelular, qntdCelular)
-         celulares.add(celular);
+ 
+public List<Celular> celulares = new LinkedList<>();
+public List<Tablet> tablets = new LinkedList<>();
+
+    // Cadastrando Itens
+public void cadastrarCelular(String nomeCelular, String marcaCelular, double precoCelular, int qntdCelular) {
+    Celular celular = new Celular(nomeCelular, marcaCelular, precoCelular, qntdCelular);
+    celulares.add(celular);
+}
+public boolean cadastrarTablet(String nomeTablet, String marcaTablet, double precoTablet, int qntdTablet) {
+    Tablet tablet = new Tablet(nomeTablet, marcaTablet, precoTablet, qntdTablet);
+    tablets.add(tablet);
+    System.out.println(nomeTablet + " adicionado!");
+    return true;
+}
+
+    // Metodo Buscar Celular
+public Celular buscarCelular(String nomeCelular) {
+    for(Celular celular : celulares){
+        if (celular.getnomeCelular().equalsIgnoreCase(nomeCelular)){
+            System.out.println(celular + " encontrado!");
+            return celular;
         }
-
-        public boolean cadastrarTablet(String modeloTablet, String marcaTablet, int qntdTable
-         Tablet tablet = new Tablet(modeloTablet, marcaTablet, qntdTablet)
-         tablets.add(tablet)
-         System.out.println(modeloTablet + " adicionado!")
-         return true
-     
-
-    
-            public Celular buscarCelular(String modeloCe
-        for (Celular celular : celulares) {
-            if (celular.getModeloCelular().equalsIgnoreCase(modeloCelular)) {
-                System.out.println(celular + " encontrado!");
-                return celular;
-            }
-        }
-        System.out.println("Celular não encontrado!");
-        return null;
     }
+    System.out.println("Celular não encontrado!");
+    return null;
+}
 
     // Metodo Buscar Tablet
-    public Tablet buscarTablet(String modeloTablet) {
-        for (Tablet tablet : tablets) {
-            if (tablet.getModeloTablet().equalsIgnoreCase(modeloTablet)) {
-                System.out.println(tablet + " encontrado!");
-                return tablet;
-            }
+public Tablet buscarTablet(String nomeTablet){
+    for(Tablet tablet : tablets){
+        if (tablet.getnomeTablet().equalsIgnoreCase(nomeTablet)) {
+            System.out.println(tablet + " encontrado!");
+            return tablet;
         }
-        System.out.println("Tablet não encontrado!");
-        return null;
     }
+    System.out.println("Tablet não encontrado!");
+    return null;
+}
 }
