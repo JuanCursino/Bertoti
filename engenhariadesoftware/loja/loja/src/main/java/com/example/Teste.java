1bd8d88fe75f6de2132c7dd1b52648e725c6ecc1
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2024-05-27 19:51:19 -0300

    Update Teste.java

--- engenhariadesoftware/loja/loja/src/main/java/com/example/Teste.java
+++ engenhariadesoftware/loja/loja/src/main/java/com/example/Teste.java
@@ -11,24 +11,24 @@ Loja lojinha = new Loja();
 
 @Test
 public void testeCadastrarCelular(){
-    lojinha.cadastrarCelular("iPhone", "Apple", "12 Pro", 5000);
-    lojinha.cadastrarCelular("Galaxy", "Samsung", "S20", 4000);
+    lojinha.cadastrarCelular("iPhone", "Apple", "12 Pro", 5);
+    lojinha.cadastrarCelular("Galaxy", "Samsung", "S20", 4);
 
     assertTrue(lojinha.celulares.getLast().getNomeCelular().equals("Galaxy"));
 }
 
 @Test
 public void testeCadastrarTablet(){
-    lojinha.cadastrarTablet("iPad", "Apple", "Air", 2000);
-    lojinha.cadastrarTablet("Galaxy Tab", "Samsung", "S7", 1500);
+    lojinha.cadastrarTablet("iPad", "Apple", "Air", 2);
+    lojinha.cadastrarTablet("Galaxy Tab", "Samsung", "S7", 1);
     
     assertFalse(lojinha.tablets.getFirst().getNomeTablet().equals("iPad"));
 }
 
 @Test
 public void testeBuscarCelular(){
-    lojinha.cadastrarCelular("iPhone", "Apple", "12 Pro", 5000);
-    lojinha.cadastrarCelular("Galaxy", "Samsung", "S20", 4000);
+    lojinha.cadastrarCelular("iPhone", "Apple", "12 Pro", 5);
+    lojinha.cadastrarCelular("Galaxy", "Samsung", "S20", 4);
     lojinha.buscarCelular("Galaxy");
     
     assertEquals("Galaxy", lojinha.buscarCelular("Galaxy").getNomeCelular());
@@ -36,8 +36,8 @@ public void testeBuscarCelular(){
 
 @Test
 public void testeBuscarTablet(){
-    lojinha.cadastrarTablet("iPad", "Apple", "Air", 2000);
-    lojinha.cadastrarTablet("Galaxy Tab", "Samsung", "S7", 1500);
+    lojinha.cadastrarTablet("iPad", "Apple", "Air", 2);
+    lojinha.cadastrarTablet("Galaxy Tab", "Samsung", "S7", 15);
     lojinha.buscarTablet("iPad");
     
     assertEquals("iPad", lojinha.buscarTablet("iPad").getNomeTablet());
